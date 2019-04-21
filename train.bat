291876ae3df1a517a93cb2ac67b4515fa1ae698c
FurryFur <lance.e.chaney@gmail.com>
2019-04-21 20:21:36 +1200

    Set training params to be same as for default wavegan.

--- train.bat
+++ train.bat
@@ -4,10 +4,7 @@ python train_wavegan.py train .\train_swordfighter ^
 --data_first_slice ^
 --data_fast_wav ^
 --data_num_channels 2 ^
---wavegan_disc_nupdates 2 ^
 --wavegan_kernel_len 24 ^
---wavegan_dim 64 ^
---train_batch_size 64 ^
 --train_save_secs 600 ^
 --train_summary_secs 15
 
