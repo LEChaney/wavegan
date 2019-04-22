b18b66b5d9637fabdd10747273d907cb62cdc309
FurryFur <lance.e.chaney@gmail.com>
2019-04-22 14:07:04 +1200

    Extended existing WaveGAN model by only 'adding' residual blocks.

---
+++ incept.bat
@@ -0,0 +1,2 @@
+set CUDA_VISIBLE_DEVICES="-1"
+python train_wavegan.py incept ./train_sc09
---
+++ train_sc09.bat
@@ -0,0 +1,7 @@
+python train_wavegan.py train .\train_sc09 ^
+--data_dir .\data\sc09\train ^
+--use_resnet ^
+--data_first_slice ^
+--data_fast_wav ^
+--train_save_secs 600 ^
+--train_summary_secs 15
