00202b91b1354e25f7ee18ff598f8178cbab49be
FurryFur <lance.e.chaney@gmail.com>
2019-04-25 20:44:07 +1200

    Setup training params for gamemaster audio

--- train_gamemaster.bat
+++ train_gamemaster.bat
@@ -1,9 +1,12 @@
 python train_wavegan.py train .\train_gamemaster ^
 --data_dir .\data\Gamemaster_Audio_16KHz ^
+--use_resnet ^
+--use_conditioning ^
 --data_first_slice ^
 --data_fast_wav ^
 --data_num_channels 2 ^
---wavegan_dim 32 ^
---data_slice_len 65536 ^
+--wavegan_disc_nupdates 1 ^
+--wavegan_disc_phaseshuffle 1 ^
+--wavegan_dim 64 ^
 --train_save_secs 600 ^
 --train_summary_secs 15
