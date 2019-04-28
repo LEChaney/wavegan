fd778aa99a19b52933618f0cae60f5fef2ca62f7
FurryFur <lance.e.chaney@gmail.com>
2019-04-29 00:33:19 +1200

    Training with larger batch size and "two time-scale update rule" i.e. different learning rates for generator / discriminator.

--- train_sc09.bat
+++ train_sc09.bat
@@ -8,5 +8,6 @@ python train_wavegan.py train .\train_sc09 ^
 --wavegan_disc_nupdates 1 ^
 --wavegan_disc_phaseshuffle 1 ^
 --wavegan_dim 64 ^
+--train_batch_size 96 ^
 --train_save_secs 600 ^
 --train_summary_secs 15
