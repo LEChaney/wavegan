f46c7e50559dd01e56192697337848ce4f3d6486
FurryFur <lance.e.chaney@gmail.com>
2019-05-26 14:39:13 +1200

    Updated gamemaster audio training script to use new functionality.

--- train_gamemaster.bat
+++ train_gamemaster.bat
@@ -1,14 +1,24 @@
 python train_wavegan.py train .\train_gamemaster ^
 --data_dir .\data\Gamemaster_Audio_16KHz ^
 --use_resnet ^
---use_maxout ^
+--use_ortho_init ^
 --use_conditioning ^
+--use_skip_z ^
+--use_spec_norm ^
 --data_first_slice ^
 --data_fast_wav ^
 --data_num_channels 2 ^
+--wavegan_genr_upsample zeros ^
+--wavegan_loss wgan-gp ^
 --wavegan_disc_nupdates 1 ^
 --wavegan_disc_phaseshuffle 1 ^
+--wavegan_latent_dim 100 ^
+--embedding_dim 100 ^
+--wavegan_kernel_len 25 ^
 --wavegan_dim 64 ^
---train_batch_size 110 ^
+--train_batch_size 256 ^
+--n_minibatches 2 ^
+--n_macro_patches 4 ^
+--n_micro_patches 4 ^
 --train_save_secs 600 ^
 --train_summary_secs 15
